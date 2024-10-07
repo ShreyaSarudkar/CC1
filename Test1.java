70ded0bd2d2eddfe91c5e48f18077a42eb12de64
Shreya Sarudkar <120292617+ShreyaSarudkar@users.noreply.github.com>
2024-10-07 10:19:53 +0530

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,7 @@
+class Test1
+{
+  public static void main (String args[])
+  {
+    System.out.println("This Is A Program From My Git Hub Account And Excecuting Using Googlr Cloud Console.");
+  }
+}
